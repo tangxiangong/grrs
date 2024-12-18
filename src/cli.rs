7315919bd50b8f885f854e3f14d2b23a037126b0
tangxiangong <tangxiangong@gmail.com>
2024-12-18 22:14:32 +0800

    空行

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,9 @@
-use std::fs;
-use std::io::{self, Write, BufWriter, BufRead, BufReader};
-use std::path::PathBuf;
-use structopt::StructOpt;
 use anyhow::{Context, Result};
 use colored::Colorize;
-
+use std::fs;
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -23,19 +22,26 @@ pub fn work(args: &Cli) -> Result<()> {
     let mut handle = BufWriter::new(stdout.lock());
     // let file = fs::File::open(&args.path).expect("could not open the file");
 
-    let file = fs::File::open(&args.path)
-        .with_context(|| format!("could not open file {}", path))?;
+    let file =
+        fs::File::open(&args.path).with_context(|| format!("could not open file {}", path))?;
     let reader = BufReader::new(file);
     let mut counter = 0;
     let red_text = args.pattern.clone().red().to_string();
-    
+
     for (num, result) in reader.lines().enumerate() {
         let line = result.with_context(|| format!("could not read file {}", path))?;
         if line.contains(&args.pattern) {
             let color_line = line.replace(&args.pattern, &red_text);
             counter += 1;
-            writeln!(handle, "No.{}, Line {}: \"{}\"", counter, num+1, color_line)
-                .with_context(|| "error")?;
+            writeln!(
+                handle,
+                "No.{}, Line {}: \"{}\"",
+                counter,
+                num + 1,
+                color_line
+            )
+            .with_context(|| "error")?;
+            writeln!(handle, "\t")?;
         }
     }
     handle.flush()?;
